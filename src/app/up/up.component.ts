0847bcc0031f7e396be9f089ce4f3c810f2c769b
Júnior <clovispilgerjunior@hotmail.com>
2023-09-25 20:41:50 -0300

    Up front-end

--- src/app/up/up.component.ts
+++ src/app/up/up.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-up',
   templateUrl: './up.component.html',
   styleUrls: ['./up.component.css']
 })
-export class UpComponent {
+export class UpComponent implements OnInit{
+
+  constructor(
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.navigate(['up'])
+  }
 
 }
